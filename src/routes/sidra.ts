63c546735c0220734b7e03cafc2aa9ce1a3d6df7
MatheusGASource <matheusgasource@gmail.com>
2017-01-09 17:55:52 -0200

    Tratando resposta da api sidra

--- src/routes/sidra.ts
+++ src/routes/sidra.ts
@@ -8,16 +8,16 @@ export const sidra: IRouteConfiguration = {
   path: '/sidra',
   handler: (request: Request, reply: IReply) => {
     // Utilizando api sidra
-    Wreck.get('http://api.sidra.ibge.gov.br/values/t/1612/n1/1/n2/all/c81/2702/p/all/v/214/f/n',
+    Wreck.get('http://api.sidra.ibge.gov.br/values/t/1612/n1/1/n2/all/c81/2702/p/last/v/214/f/n',
     {json: true}, (err: any, response: any, payload: any) => {
       // Respondendo saida da api sidra padronizada
-      reply({
-        Regiao: payload[1].D1N,
-        Produto: payload[1].D2N,
-        Ano: payload[1].D3N,
-        Unidade: payload[1].MN,
-        Valor: payload[1].V
-      })
+      const results: any = payload.map((data: any) => ({
+        ano: data.D3N,
+        produto: data.D2N,
+        unidate: data.MN,
+        valor: data.V
+      }))
+      reply(results)
     })
   }
 }
